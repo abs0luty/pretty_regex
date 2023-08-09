65ee111bc7582f8dbe69bfe0a51776604d295379
Adi Salimgereev <adisalimgereev@gmail.com>
2023-08-09 10:06:14 +0600

    feat: add crate level docs

--- pretty_regex/src/lib.rs
+++ pretty_regex/src/lib.rs
@@ -1,3 +1,101 @@
+//! # 🔮 Write readable regular expressions
+//!
+//! The crate provides a clean and readable way of writing your regex in the Rust programming language:
+//!
+//! <table>
+//! <tr>
+//! <td>
+//!
+//! Without `pretty_regex`
+//!  
+//! </td>
+//! <td>
+//!
+//! With `pretty_regex`
+//!
+//! </td>
+//! </tr>
+//!
+//! <tr>
+//! <td>
+//!
+//! ```reg
+//! \d{5}(-\d{4})?
+//! ```
+//!
+//! </td>
+//! <td>
+//!
+//! ```ignore
+//! digit()
+//!  .repeats(5)
+//!  .then(
+//!    just("-")
+//!     .then(digit().repeats(4))
+//!     .optional()
+//!  )
+//! ```
+//!
+//! </td>
+//! </tr>
+//! <tr>
+//! <td>
+//!
+//! ```reg
+//! ^(?:\d){4,}(?:(?:\-)(?:\d){2,}){2,}$
+//! ```
+//!
+//! </td>
+//! <td>
+//!
+//! ```ignore
+//! beginning()
+//!   .then(digit().repeats(4))
+//!   .then(
+//!     just("-")
+//!       .then(digit().repeats(2))
+//!       .repeats(2)
+//!   )
+//!   .then(ending())
+//! ```
+//!
+//! </td>
+//! </tr>
+//!
+//! <tr>
+//! <td>
+//!
+//! ```reg
+//! rege(x(es)?|xps?)
+//! ```
+//!
+//! </td>
+//! <td>
+//!
+//! ```ignore
+//! just("rege")
+//!   .then(one_of(&[
+//!     just("x").then(just("es").optional()),
+//!     just("xp").then(just("s").optional()),
+//!   ]))
+//! ```
+//!
+//! </td>
+//! </tr>
+//! </table>
+//!
+//! # How to use the crate?
+//!
+//! To convert a `PrettyRegex` struct which is constructed using all these `then`, `one_of`, `beginning`, `digit`, etc. functions into
+//! a real regex (from `regex` crate), you can call `to_regex` or `to_regex_or_panic`:
+//!
+//! ```
+//! use pretty_regex::digit;
+//! let regex = digit().to_regex_or_panic();
+//!
+//! assert!(regex.is_match("3"));
+//! ```
+
 use regex::{escape, Regex};
 use unicode::Category;
 
